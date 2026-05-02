ad81514e5eb615c7609e1aabf97aa3518d468038
ArdhiaSalwaIndriani <ardhiasalwa@gmail.com>
2026-05-02 10:11:20 +0700

    Delete PrakTAM_2457051004 directory

--- PrakTAM_2457051004/app/src/main/java/network/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.praktam_2457051004.network
-
-import Model.Fasilink
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("https://gist.githubusercontent.com/ArdhiaSalwaIndriani/5d850650eb5880ed87e4f343e84c5cbd/raw/5cc52a5590d8bb9a80e28d5da2e4ba3ecb8832e3/fasilink.json")
-    suspend fun getLaporan(): List<Fasilink>
-}
